2321d355ceaa7cdad3722b5c7ba81a15fc57fe03
Brendon <bd00551619@techmahindra.com>
2020-11-07 16:22:42 +0530

    table ui

--- src/app/commonComponents/table-headers/table-headers.component.ts
+++ src/app/commonComponents/table-headers/table-headers.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table-headers',
   templateUrl: './table-headers.component.html',
   styleUrls: ['./table-headers.component.scss']
 })
-export class TableHeadersComponent implements OnInit {
+export class TableHeadersComponent {
+  @Input() headerGap = 0;
+  @Input() headerData = [];
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeTableHeaderData } from './employee-table-header-data';
 
+const EMPLOYEE_DATA = [
+  {
+    id: 1,
+    name: 'Jhon',
+    phone: '9999999999',
+    address: {
+      city: 'Pune',
+      address_line1: 'ABC road',
+      address_line2: 'XYZ building',
+      postal_code: '12455'
+    }
+  },
+  {
+    id: 2,
+    name: 'Jacob',
+    phone: 'AZ99A99PQ9',
+    address: {
+      city: 'Pune',
+      address_line1: 'PQR road',
+      address_line2: 'ABC building',
+      postal_code: '13455'
+    }
+  },
+  {
+    id: 3,
+    name: 'Ari',
+    phone: '145458522',
+    address: {
+      city: 'Mumbai',
+      address_line1: 'ABC road',
+      address_line2: 'XYZ building',
+      postal_code: '12455'
+    }
+  }
+];
+
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -9,12 +45,13 @@ import { EmployeeTableHeaderData } from './employee-table-header-data';
 export class EmployeeListComponent implements OnInit {
   columnGap = 0;
   columnHeaderData = [];
+  employeeData = [];
 
   constructor() { }
 
   ngOnInit() {
     this.columnGap = EmployeeTableHeaderData.columnGap;
     this.columnHeaderData = EmployeeTableHeaderData.columnHeaderData;
+    this.employeeData = EMPLOYEE_DATA;
   }
-
 }
