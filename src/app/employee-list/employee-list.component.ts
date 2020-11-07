96b483cf8d72119da20c14715b731b502bb37310
Brendon <bd00551619@techmahindra.com>
2020-11-07 18:09:31 +0530

    integration changes

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeTableHeaderData } from './employee-table-header-data';
 import { EmployeeService } from './../services/employee.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-employee-list',
@@ -31,7 +32,19 @@ export class EmployeeListComponent implements OnInit {
   }
 
   checkEmployeeResponse(employeeData) {
-    employeeData.forEach(data => { });
+    employeeData.forEach(data => {
+      data.id = this.checkField(_.get(data, '_id', ''));
+      data.name = this.checkField(_.get(data, 'name', ''));
+      data.phone = this.checkField(_.get(data, 'phone', ''));
+      data.city = this.checkField(_.get(data, 'address.city', ''));
+      data.address_line1 = this.checkField(_.get(data, 'address.address_line1', ''));
+      data.address_line2 = this.checkField(_.get(data, 'address.address_line2', ''));
+      data.postal_code = this.checkField(_.get(data, 'address.postal_code', ''));
+    });
     return employeeData;
+  }
+
+  checkField(value) {
+    return value || 'NA';
   }
 }
